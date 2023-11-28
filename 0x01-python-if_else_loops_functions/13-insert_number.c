3a2dc69277db0db203761cf1981e0429ca16a11e
tastaw23 <younesmekki2@gmail.com>
2023-11-28 10:00:28 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,34 +1,71 @@
-#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * insert_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Return: If the function fails - NULL.
- * Otherwise - a pointer to the new node.
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *node = *head, *new;
+    listint_t *new_node, *current, *prev;
 
-        new = malloc(sizeof(listint_t));
-        if (new == NULL)
-                return (NULL);
-        new->n = number;
+    if (head == NULL)
+        return (NULL);
 
-        if (node == NULL || node->n >= number)
-        {
-                new->next = node;
-                *head = new;
-                return (new);
-        }
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-        while (node && node->next && node->next->n < number)
-                node = node->next;
+    new_node->n = number;
+    new_node->next = NULL;
 
-        new->next = node->next;
-        node->next = new;
+    current = *head;
+    prev = NULL;
 
-        return (new);
+    while (current != NULL && current->n < number)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    if (prev == NULL)
+    {
+        new_node->next = *head;
+        *head = new_node;
+    }
+    else
+    {
+        prev->next = new_node;
+        new_node->next = current;
+    }
+
+    return (new_node);
 }
+
+/* Example usage */
+int main(void)
+{
+    listint_t *head = NULL;
+
+    insert_node(&head, 3);
+    insert_node(&head, 1);
+    insert_node(&head, 5);
+
+    return 0;
+}
+
