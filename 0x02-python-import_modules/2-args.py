c1574ff0b561146a6f2d4499309283045da399bc
tastaw23 <younesmekki2@gmail.com>
2023-11-30 02:11:13 -0800

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    plural_s = 's' if argc != 1 else ''
+
+    print("{} argument{}{}{}".format(argc, plural_s, '.' if argc == 0 else ':', ''))
+
+    for i in range(argc):
+        print("{}: {}".format(i + 1, argv[i + 1]))
