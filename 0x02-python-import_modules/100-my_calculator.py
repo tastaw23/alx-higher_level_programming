eb2711d78e6c829b734d36cb81a408b7e7be6c5b
tastaw23 <younesmekki2@gmail.com>
2023-11-30 08:41:16 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,14 +3,17 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 if __name__ == "__main__":
+    # Check the number of command-line arguments
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
+    # Parse command-line arguments
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
 
+    # Perform calculation based on the operator
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
@@ -23,5 +26,6 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    print('{} {} {} = {}'.format(a, operator, b, result))
+    # Print the result
+    print("{} {} {} = {}".format(a, operator, b, result))
 
