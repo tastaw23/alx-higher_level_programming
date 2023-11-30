5109bf9612590fbfa91abeada987abea1b4a69df
tastaw23 <younesmekki2@gmail.com>
2023-11-30 08:22:48 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argc = len(argv) - 1
     plural_s = 's' if argc != 1 else ''
 
-    print("{} argument{}{}{}".format(argc, plural_s, '.' if argc == 0 else ':', ''))
+    print('{} argument{}{}{}'.format(argc, plural_s, '.' if argc == 0 else ':', ''))
 
     for i in range(argc):
-        print("{}: {}".format(i + 1, argv[i + 1]))
+        print('{}: {}'.format(i + 1, argv[i + 1]))
