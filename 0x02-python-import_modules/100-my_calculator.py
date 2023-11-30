9c62f3a62d4785c1af0896b607deb06e19816908
tastaw23 <younesmekki2@gmail.com>
2023-11-30 08:38:36 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -23,5 +23,5 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    print("{} {} {} = {}".format(a, operator, b, result))
+    print('{} {} {} = {}'.format(a, operator, b, result))
 
