5981055e231b80214b98d1c5f5eb9928083b3a43
tastaw23 <younesmekki2@gmail.com>
2023-11-30 08:16:11 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    
-a = 1
-b = 2
-
-from add_0 import add as add_0_function
-
-result = add_0_function(a, b)
-print("{} + {} = {}".format(a, b, result))
-
+    from add_0 import add
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a, b, add(a, b)))
