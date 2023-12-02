adb72087342176f8947d8af7989373bee8072434
tastaw23 <younesmekki2@gmail.com>
2023-12-02 00:20:58 -0800

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Use the first two elements of each tuple, filling with 0 if needed
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+
+    # Perform addition for each corresponding element
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
+
+# Example usage
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
+
+    print(add_tuple(tuple_a, (1,)))
+    print(add_tuple(tuple_a, ()))
+
