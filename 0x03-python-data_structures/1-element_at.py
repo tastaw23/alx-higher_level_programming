64ed2f35d418e53310036de82021f579dce2d6e0
tastaw23 <younesmekki2@gmail.com>
2023-12-01 21:17:44 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,10 +5,3 @@ def element_at(my_list, idx):
         return None
     return my_list[idx]
 
-# Example usage
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    result = element_at(my_list, idx)
-    print("Element at index {:d} is {}".format(idx, result))
-
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,13 +7,3 @@ def replace_in_list(my_list, idx, element):
     my_list[idx] = element
     return my_list
 
-# Example usage
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_element = 9
-    new_list = replace_in_list(my_list, idx, new_element)
-
-    print(new_list)
-    print(my_list)
-
