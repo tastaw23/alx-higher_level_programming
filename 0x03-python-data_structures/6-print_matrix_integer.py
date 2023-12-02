3c4c82bde6eb8cba584ebf77976d02263856bbc4
tastaw23 <younesmekki2@gmail.com>
2023-12-02 00:13:47 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,24 @@
 
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
+        print()
         return
 
-    for i, row in enumerate(matrix):
-        for j, num in enumerate(row):
+    for row in matrix:
+        for num in row:
             print("{:d}".format(num), end=" ")
-            if j < len(row) - 1:
+            if num != row[-1]:
                 print(end=" ")
-        if i < len(matrix) - 1:
-            print()
+        print()
+
+# Example usage
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
 
