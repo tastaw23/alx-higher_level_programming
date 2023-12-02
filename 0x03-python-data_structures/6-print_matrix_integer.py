017f3dbccf9ff85bee68b927fe152f3c7c974174
tastaw23 <younesmekki2@gmail.com>
2023-12-02 00:10:12 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+
     for row in matrix:
         for num in row:
             print("{:d}".format(num), end=" ")
