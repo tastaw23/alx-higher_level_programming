2cd0b48ed7e8045c76208fb456a24a25554fea1c
tastaw23 <younesmekki2@gmail.com>
2023-12-02 21:45:32 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,4 +7,4 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     new_list[idx] = element
     return new_list
-
+#########
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,4 +6,4 @@ def no_c(my_string):
         if char.lower() != 'c':
             new_string += char
     return new_string
-
+####
