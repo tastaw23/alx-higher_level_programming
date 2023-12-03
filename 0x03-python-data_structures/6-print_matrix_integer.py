46cf4f2ddb86ca0b6bf813e8e1e91c20b232542e
tastaw23 <younesmekki2@gmail.com>
2023-12-02 22:04:33 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,14 +7,4 @@ def print_matrix_integer(matrix=[]):
             if i < len(row) - 1:
                 print(end=" ")
         print()
-
-# Example usage
-if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-    print_matrix_integer(matrix)
-    print("--")
-    print_matrix_integer()
+########
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,13 +8,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Perform addition for each corresponding element
     result = (a[0] + b[0], a[1] + b[1])
     return result
-
-# Example usage
-if __name__ == "__main__":
-    tuple_a = (1, 89)
-    tuple_b = (88, 11)
-    new_tuple = add_tuple(tuple_a, tuple_b)
-    print(new_tuple)
-
-    print(add_tuple(tuple_a, (1,)))
-    print(add_tuple(tuple_a, ()))
+######
