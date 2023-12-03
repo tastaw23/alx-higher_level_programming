04b25eb1378961669f2a48ca85d5380605b5c8d5
tastaw23 <younesmekki2@gmail.com>
2023-12-02 20:24:14 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -8,36 +10,50 @@
 int is_palindrome(listint_t **head)
 {
     listint_t *slow = *head, *fast = *head;
-    listint_t *prev = NULL, *tmp = NULL;
+    listint_t *prev = NULL, *temp;
+    int is_palindrome = 1;
 
     if (*head == NULL || (*head)->next == NULL)
-        return (1);
+        return 1; // An empty list or a list with one element is considered a palindrome
 
+    // Use Floyd's Tortoise and Hare algorithm to find the middle of the list
     while (fast != NULL && fast->next != NULL)
     {
         fast = fast->next->next;
 
-        // Reverse the first half of the list
-        tmp = slow->next;
+        // Reverse the first half of the list while finding the middle
+        temp = slow->next;
         slow->next = prev;
         prev = slow;
-        slow = tmp;
+        slow = temp;
     }
 
-    // If the list has an odd number of elements, skip the middle one
+    // If the number of nodes is odd, move the slow pointer one step forward
     if (fast != NULL)
         slow = slow->next;
 
     // Compare the reversed first half with the second half
-    while (slow != NULL)
+    while (prev != NULL && slow != NULL)
     {
-        if (slow->n != prev->n)
-            return (0);
-
-        slow = slow->next;
+        if (prev->n != slow->n)
+        {
+            is_palindrome = 0;
+            break;
+        }
         prev = prev->next;
+        slow = slow->next;
     }
 
-    return (1);
+    // Restore the original linked list
+    prev = NULL;
+    while (*head != NULL)
+    {
+        temp = (*head)->next;
+        (*head)->next = prev;
+        prev = *head;
+        *head = temp;
+    }
+
+    return is_palindrome;
 }
 
