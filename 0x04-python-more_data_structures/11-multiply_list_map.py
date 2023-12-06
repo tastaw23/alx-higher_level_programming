79173d3e03929f3fcbb63002c2a16326b424b04a
tastaw23 <younesmekki2@gmail.com>
2023-12-06 02:11:53 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
-multiply_list_map = lambda my_list, factor: list(map(lambda x: x * factor, my_list))
+def multiply_list_map(my_list, factor):
+    """
+    Multiply each element in the list by the given factor.
 
+    Parameters:
+    - my_list (list): The input list of numbers.
+    - factor (int): The multiplication factor.
+
+    Returns:
+    - list: A new list with each element multiplied by the factor.
+    """
+    return list(map(lambda x: x * factor, my_list))
+
+#
