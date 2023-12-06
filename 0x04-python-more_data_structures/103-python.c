afec13b79762052d312861f617d1331cfe44177c
tastaw23 <younesmekki2@gmail.com>
2023-12-06 03:19:37 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,46 +1,22 @@
 #include <Python.h>
 
 void print_python_bytes(PyObject *p) {
+    if (!PyBytes_Check(p)) {
+        fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
     printf("[.] bytes object info\n");
-    printf("  size: %zd\n", PyBytes_Size(p));
-    printf("  trying string: %s\n", PyBytes_AsString(p));
+    printf("  size: %zd\n", ((PyVarObject *)p)->ob_size);
+    printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
 
     printf("  first 10 bytes: ");
-    size_t i;
-    for (i = 0; i < PyBytes_Size(p) && i < 10; ++i) {
-        printf("%02x ", (unsigned char)PyBytes_AsString(p)[i]);
+    Py_ssize_t size = ((PyVarObject *)p)->ob_size;
+    Py_ssize_t limit = (size > 10) ? 10 : size;
+
+    for (Py_ssize_t i = 0; i < limit; ++i) {
+        printf("%02x ", (unsigned char)(((PyBytesObject *)p)->ob_sval[i]));
     }
     printf("\n");
 }
-
-void print_python_list(PyObject *p) {
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %zd\n", PyList_Size(p));
-    printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
-
-    for (size_t i = 0; i < PyList_Size(p); ++i) {
-        printf("Element %zd: ", i);
-
-        PyObject *elem = PyList_GetItem(p, i);
-
-        if (PyBytes_Check(elem)) {
-            printf("bytes\n");
-            print_python_bytes(elem);
-        } else if (PyLong_Check(elem)) {
-            printf("int\n");
-        } else if (PyFloat_Check(elem)) {
-            printf("float\n");
-        } else if (PyTuple_Check(elem)) {
-            printf("tuple\n");
-        } else if (PyList_Check(elem)) {
-            printf("list\n");
-            print_python_list(elem);
-        } else if (PyUnicode_Check(elem)) {
-            printf("str\n");
-        } else {
-            printf("[ERROR] Invalid Element\n");
-        }
-    }
-}
-
 
