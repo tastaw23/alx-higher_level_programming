350759f27b37b68ff80480c8429396930aac7136
tastaw23 <younesmekki2@gmail.com>
2023-12-06 01:55:54 -0800

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    if a_dictionary:
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
+    else:
+        return None
+
+#
