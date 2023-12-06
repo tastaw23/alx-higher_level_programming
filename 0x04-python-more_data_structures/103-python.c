f021537ea274a333576a6d61f5f161060b7c4dfe
tastaw23 <younesmekki2@gmail.com>
2023-12-06 03:32:49 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,28 +1,72 @@
 #include <Python.h>
 
+void print_python_list(PyObject *p) {
+    Py_ssize_t i, size;
+    PyObject *item;
+
+    if (!PyList_Check(p)) {
+        fprintf(stderr, "[ERROR] Invalid Python List\n");
+        return;
+    }
+
+    size = PyList_Size(p);
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %zd\n", size);
+    printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; ++i) {
+        item = PyList_GetItem(p, i);
+        printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
 void print_python_bytes(PyObject *p) {
+    Py_ssize_t size, i;
+    char *str;
+
     if (!PyBytes_Check(p)) {
         fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
         return;
     }
 
+    size = PyBytes_Size(p);
+    str = PyBytes_AsString(p);
+
     printf("[.] bytes object info\n");
-    printf("  size: %zd\n", ((PyVarObject *)p)->ob_size);
-    
-    // Print the string representation using a loop
-    printf("  trying string: ");
-    for (Py_ssize_t i = 0; i < ((PyVarObject *)p)->ob_size; ++i) {
-        printf("%c", ((PyBytesObject *)p)->ob_sval[i]);
-    }
-    printf("\n");
+    printf("  size: %zd\n", size);
+    printf("  trying string: %s\n", str);
 
     printf("  first 10 bytes: ");
-    Py_ssize_t size = ((PyVarObject *)p)->ob_size;
-    Py_ssize_t limit = (size > 10) ? 10 : size;
-
-    for (Py_ssize_t i = 0; i < limit; ++i) {
-        printf("%02x ", (unsigned char)(((PyBytesObject *)p)->ob_sval[i]));
+    for (i = 0; i < 10 && i < size; ++i) {
+        printf("%02x ", (unsigned char)str[i]);
     }
     printf("\n");
+}
+
+int main() {
+    // Example usage:
+    Py_Initialize();
+
+    // Create a Python list
+    PyObject *myList = PyList_New(3);
+    PyList_SetItem(myList, 0, PyLong_FromLong(42));
+    PyList_SetItem(myList, 1, PyUnicode_FromString("Hello"));
+    PyList_SetItem(myList, 2, PyFloat_FromDouble(3.14));
+
+    // Create a Python bytes object
+    PyObject *myBytes = PyBytes_FromStringAndSize("PythonBytes", 11);
+
+    // Print info about the Python list and bytes object
+    print_python_list(myList);
+    print_python_bytes(myBytes);
+
+    // Cleanup
+    Py_DECREF(myList);
+    Py_DECREF(myBytes);
+
+    Py_Finalize();
+
+    return 0;
 }
 
