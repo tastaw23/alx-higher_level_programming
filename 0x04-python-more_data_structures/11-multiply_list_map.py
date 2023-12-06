ed874b7aee31af09ed41e59263dfc7f829df2b2d
tastaw23 <younesmekki2@gmail.com>
2023-12-06 03:57:56 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list, factor): return list(map(lambda x: x * factor, my_list))
+def multiply_list_map(my_list, factor):
+    return list(map(lambda x: x * factor, my_list))
 #
