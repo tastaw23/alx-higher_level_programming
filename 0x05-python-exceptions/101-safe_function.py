05b60f246c3ba50fd2ddd0b55b37292cec296c19
tastaw23 <younesmekki2@gmail.com>
2023-12-18 01:56:51 -0800

    py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import sys
 
 def safe_function(fct, *args):
@@ -19,14 +20,5 @@ print("result of my_div: {}".format(result))
 result = safe_function(my_div, 10, 0)
 print("result of my_div: {}".format(result))
 
-
-def print_list(my_list, len):
-    i = 0
-    while i < len:
-        print(my_list[i])
-        i += 1
-    return len
-
-result = safe_function(print_list, [1, 2, 3, 4], 10)
-print("result of print_list: {}".format(result))
+# Feel free to add more test cases or use different functions
 
