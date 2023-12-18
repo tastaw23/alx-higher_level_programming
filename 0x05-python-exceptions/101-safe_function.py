18c46891c47b2cfc9cc20daf3d8c48ea2c047f3a
tastaw23 <younesmekki2@gmail.com>
2023-12-18 02:07:08 -0800

    py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+from typing import Any, Callable, Tuple
 
-def safe_function(fct, *args):
+def safe_function(fct: Callable, *args: Tuple[Any]) -> Any:
     try:
         result = fct(*args)
         return result
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
-###
+##
