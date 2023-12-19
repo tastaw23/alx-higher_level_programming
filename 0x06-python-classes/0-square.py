77f0515b4f3cdaf36aa085c49ef39ad4021b6424
tastaw23 <younesmekki2@gmail.com>
2023-12-19 03:19:24 -0800

    py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,4 +3,10 @@
 class Square:
     """Empty class Square that defines a square."""
     pass
-##
+#!/usr/bin/python3
+Square = __import__('0-square').Square
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
+##"
