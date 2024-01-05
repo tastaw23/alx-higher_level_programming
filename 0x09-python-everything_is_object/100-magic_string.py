41e1feeaab6fd3d24adcc4d98e3a756389f3357d
tastaw23 <younesmekki2@gmail.com>
2024-01-05 07:41:05 -0800

    py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-
 def magic_string():
-    """
-    Returns a string "BestSchool" n times the number of the iteration.
-
-    Format: see example
-    """
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
-###
