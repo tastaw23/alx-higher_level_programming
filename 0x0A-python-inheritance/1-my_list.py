950a36ac616b0f117bf932f8d3e309ce1da15cce
tastaw23 <younesmekki2@gmail.com>
2024-01-08 04:17:37 -0800

    py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,35 +1,10 @@
 #!/usr/bin/python3
+"""Defines an inherited list class MyList."""
+
 
 class MyList(list):
-    """
-    MyList class inherits from the list class and adds a print_sorted method.
-    """
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """
-        Public instance method to print the list in sorted (ascending order).
-
-        This method modifies the behavior of the built-in print function.
-
-        Example:
-        >>> my_list = MyList([1, 4, 2, 3, 5])
-        >>> print(my_list)
-        [1, 4, 2, 3, 5]
-        >>> my_list.print_sorted()
-        [1, 2, 3, 4, 5]
-        """
+        """Print a list in sorted ascending order."""
         print(sorted(self))
-
-# Example usage
-if __name__ == "__main__":
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
-
