06b7b91d8fb98431b8b6b108f71620ceea840dfe
tastaw23 <younesmekki2@gmail.com>
2024-01-08 04:14:08 -0800

    py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -11,17 +11,3 @@ def lookup(obj):
     - A list containing the attributes and methods of the object.
     """
     return dir(obj)
-
-# Example usage
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
-
