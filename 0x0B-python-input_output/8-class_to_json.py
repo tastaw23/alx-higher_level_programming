e4318d2834b49269f5d7fa25eafaa946c6b5afb4
tastaw23 <younesmekki2@gmail.com>
2024-01-09 02:16:31 -0800

    py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -27,30 +27,3 @@ def class_to_json(obj):
             attributes[key] = str(value)
 
     return attributes
-
-if __name__ == "__main__":
-    # Test with MyClass from 8-my_class.py
-    MyClass = __import__('8-my_class').MyClass
-    class_to_json = __import__('8-class_to_json').class_to_json
-
-    m = MyClass("John")
-    m.number = 89
-    print(type(m))
-    print(m)
-
-    mj = class_to_json(m)
-    print(type(mj))
-    print(mj)
-
-    # Test with MyClass from 8-my_class_2.py
-    MyClass = __import__('8-my_class_2').MyClass
-    class_to_json = __import__('8-class_to_json').class_to_json
-
-    m = MyClass("John")
-    m.win()
-    print(type(m))
-    print(m)
-
-    mj = class_to_json(m)
-    print(type(mj))
-    print(mj)
