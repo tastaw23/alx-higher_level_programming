d74c8ed0f2128c7397c330b1499c12b344ab1b1d
tastaw23 <younesmekki2@gmail.com>
2024-02-13 08:50:30 -0800

    air

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,8 +2,6 @@
 -- Script to print the full description of the table 'first_table' in the specified database
 
 -- Database name passed as an argument to the mysql command
-USE `$1`;
-
 -- Printing the full description of 'first_table'
 SHOW CREATE TABLE first_table;
 
