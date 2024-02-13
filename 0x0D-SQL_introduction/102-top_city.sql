91e524dd1355094cde8d8f1f380f7785fbfe9d9b
tastaw23 <younesmekki2@gmail.com>
2024-02-13 09:33:52 -0800

    air

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,3 @@
+-- List top 3 cities
 -- Execute: cat 102-top_city.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN ('July', 'August') GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
