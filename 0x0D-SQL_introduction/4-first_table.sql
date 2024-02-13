eeb787c694dc6fe41ec20e2a064e05396338f8e8
tastaw23 <younesmekki2@gmail.com>
2024-02-13 08:43:42 -0800

    air

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,8 +2,6 @@
 -- Script to create the table 'first_table' in the specified database
 
 -- Database name passed as an argument to the mysql command
-USE `$1`;
-
 -- Creating 'first_table' if it doesn't exist
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
