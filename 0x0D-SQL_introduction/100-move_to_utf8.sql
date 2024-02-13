c9b4fbd6f8a5111e97e23311ba51f877f05e6b9b
tastaw23 <younesmekki2@gmail.com>
2024-02-13 09:07:15 -0800

    air

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,8 @@
--- Connect to MySQL server
-
-USE hbtn_0c_0
--- Change the character set and collation for hbtn_0c_0 database
+-- 100. Move to UTF8
+-- Script to convert hbtn_0c_0 database, first_table table, and name field to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Change the character set and collation for first_table table
+USE hbtn_0c_0
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Change the character set and collation for name field in first_table
 ALTER TABLE first_table MODIFY COLUMN name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
