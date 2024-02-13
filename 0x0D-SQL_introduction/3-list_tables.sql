5d848ed098ce6c0f6ee65eda6b3a91ee403eb3b3
tastaw23 <younesmekki2@gmail.com>
2024-02-13 08:41:40 -0800

    air

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,9 +1,6 @@
 -- 3. List tables of a database
 -- Script to list all tables of a specified database
 
--- Database name passed as an argument to the mysql command
-USE `$1`;
-
 -- Displaying all tables in the specified database
 SHOW TABLES;
 
