d4ba16d6b8a92f2f388257a64701306072f547b0
tastaw23 <younesmekki2@gmail.com>
2024-02-14 02:59:22 -0800

    air

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- Task: List all shows without the genre Comedy from hbtn_0d_tvshows
+
+-- Database name will be passed as an argument
+
+-- List all shows without the genre Comedy
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name <> 'Comedy' OR tv_genres.name IS NULL
+ORDER BY tv_shows.title ASC;
+
