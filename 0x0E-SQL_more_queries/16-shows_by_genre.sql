8c8dfecd53b03bea2b268d2ea50daad1e4027387
tastaw23 <younesmekki2@gmail.com>
2024-02-14 02:53:38 -0800

    air

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- Task: List all shows and their linked genres from hbtn_0d_tvshows
+
+-- Database name will be passed as an argument
+
+-- List all shows and their linked genres
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+
