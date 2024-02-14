305d9ef3511e2145229541397fdda059bd3c39a8
tastaw23 <younesmekki2@gmail.com>
2024-02-14 01:48:51 -0800

    air

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -5,11 +5,8 @@
 -- Create database hbtn_0d_2 if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Use the database hbtn_0d_2
-USE hbtn_0d_2;
-
--- Create table force_name if it does not exist
-CREATE TABLE IF NOT EXISTS force_name (
+-- Create table force_name in the hbtn_0d_2 database
+CREATE TABLE IF NOT EXISTS hbtn_0d_2.force_name (
     id INT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
