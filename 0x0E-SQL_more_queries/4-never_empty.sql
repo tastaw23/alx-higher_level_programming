f6c107ec014b0e29415114f23eae49b55b2384fe
tastaw23 <younesmekki2@gmail.com>
2024-02-14 01:58:29 -0800

    air

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -3,7 +3,7 @@
 -- Task Description: Create table id_not_null in the specified database
 
 -- Create table id_not_null if it does not exist
-CREATE TABLE IF NOT EXISTS hbtn_0d_2.id_not_null (
+CREATE TABLE IF NOT EXISTS not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
 );
