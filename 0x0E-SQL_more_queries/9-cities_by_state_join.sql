c519438346545c8e7dd0828aafe3e3afc356cdbe
tastaw23 <younesmekki2@gmail.com>
2024-02-14 02:33:22 -0800

    air

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,7 @@
+-- Task: List all cities in the database hbtn_0d_usa with associated state names
+
+-- Database name will be passed as an argument
+
 -- List all cities with associated state names
 SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
 FROM cities
