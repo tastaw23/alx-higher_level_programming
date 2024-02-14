2cf5307398460f9d21f34885de80a244ed6cee64
tastaw23 <younesmekki2@gmail.com>
2024-02-14 02:11:47 -0800

    air

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,11 @@
--- Task: Create the hbtn_0d_usa database and the states table if they don't exist
+-- creates the database hbtn_0d_usa and the table states on your MySQL server.
+   -- states description:
+      -- id INT unique - auto generated, cant be null, is a primary key
+      -- name VARCHAR(256) - can't be null
+   -- If the table hbtn_0d_usa already exists, script should not fail
+   -- If the table states already exists, script should not fail
 
--- Check if the hbtn_0d_usa database exists, create it if not
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256) NOT NULL);
