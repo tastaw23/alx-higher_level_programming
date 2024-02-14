a9c6bc7abf28a51a38b669f5d22bf9ad05d598bc
tastaw23 <younesmekki2@gmail.com>
2024-02-14 02:10:45 -0800

    air

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,16 +2,7 @@
 
 -- Check if the hbtn_0d_usa database exists, create it if not
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Switch to the hbtn_0d_usa database
-USE hbtn_0d_usa;
-
--- Check if the states table already exists, create it if not
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
-
--- Example INSERT query
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
-
