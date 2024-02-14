b5f23a0d6f3ecf02474e06fea3dbf9f92127b2e2
tastaw23 <younesmekki2@gmail.com>
2024-02-14 01:35:11 -0800

    air

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,17 @@
+-- 3-force_name.sql
+
+-- Task Description: Create database hbtn_0d_2 and table force_name
+
+-- Create database hbtn_0d_2 if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Use the database hbtn_0d_2
+USE hbtn_0d_2;
+
 -- Create table force_name if it does not exist
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
+
