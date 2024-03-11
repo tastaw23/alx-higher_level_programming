777f4cfcf8eba6d31cf9e8fc5a12cfdae646c651
tastaw23 <younesmekki2@gmail.com>
2024-03-11 07:03:16 -0700

    air

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-function factorial(n) {
-  if (isNaN(n)) {
-    return 1;
-  } else if (n === 0) {
+function factorial (n) {
+  if ((isNaN(n)) || (n === 1)) {
     return 1;
   } else {
     return n * factorial(n - 1);
   }
 }
 
-console.log(factorial(num));
+console.log(factorial(parseInt(process.argv[2])));
