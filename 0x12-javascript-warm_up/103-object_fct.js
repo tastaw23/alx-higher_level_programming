e5cd380f7c9e160bf4b75ceda6587c4766e6a7a0
tastaw23 <younesmekki2@gmail.com>
2024-03-11 06:38:09 -0700

    air

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
+//  new function incr that increments the integer value.
 
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
+  value: 12
 };
-
 console.log(myObject);
+myObject.incr = function () {
+  myObject.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
