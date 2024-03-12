21907fab1cb3ca0e98310b4877c86584ae3f576e
tastaw23 <younesmekki2@gmail.com>
2024-03-12 10:12:08 -0700

    air

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    }
-
-    this.width = w;
-    this.height = h;
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-}
-
-module.exports = Rectangle;
+};
