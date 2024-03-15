6629c84c183fa5171868d67ac140d64013c37dd6
tastaw23 <younesmekki2@gmail.com>
2024-03-15 06:48:21 -0700

    air

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get MySQL username, password, and database name from command-line arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        cursor = db.cursor()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    # Query to select all states sorted by id in ascending order
+    query = "SELECT * FROM states ORDER BY id ASC;"
+
+    # Execute the query
+    try:
+        cursor.execute(query)
+        states = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        cursor.close()
+        db.close()
+        sys.exit(1)
+
+    # Display results
+    for state in states:
+        print(state)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
