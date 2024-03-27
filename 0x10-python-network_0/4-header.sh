2b47a05e5a142682077144e7579e070fb2fdb542
tastaw23 <younesmekki2@gmail.com>
2024-03-27 07:10:19 -0700

    air

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes in a URL as an argument, sends a GET request to the URL,
-# and displays the body of the response. It also sends a header variable X-School-User-Id with the value 98.
-
-curl -sH "X-School-User-Id: 98" "$1"
+# script to send custom headers to servers
+curl -s -H "X-School-User-Id: 98" "$1"
