8169306f80f3285470f2a43ae3d51b09122d9bf9
tastaw23 <younesmekki2@gmail.com>
2024-03-27 06:59:44 -0700

    air

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes a URL as input, sends a request to that URL using curl,
-# and displays the size of the body of the response in bytes
-
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
