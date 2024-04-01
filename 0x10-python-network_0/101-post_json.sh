bf847ff1f7aeb475c29cb4deb1b4959b72fd509e
tastaw23 <younesmekki2@gmail.com>
2024-04-01 06:09:01 -0700

    air

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL with the contents of a file as the request body
-# Displays the body of the response
-json_file="$2"
-if [ -f "$json_file" ]; then
-    response=$(curl -s -X POST -H "Content-Type: application/json" -d @"$json_file" "$1")
-    if [[ $response == *"Valid JSON"* ]]; then
-        echo "Valid JSON"
-    else
-        echo "Not a valid JSON"
-    fi
-else
-    echo "File not found"
-fi
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
