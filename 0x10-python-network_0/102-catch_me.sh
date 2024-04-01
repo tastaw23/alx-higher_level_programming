95da9201e731de36245b67dee82054d25ba63b11
tastaw23 <younesmekki2@gmail.com>
2024-04-01 06:11:51 -0700

    air

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me to cause the server to respond with "You got me!"
-curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
-
-
 def find_peak(list_of_integers):
     """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
-        return None
-
     size = len(list_of_integers)
-    if size == 1:
+    if size == 0:
+        return None
+    elif size == 1:
         return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    left, right = 0, size - 1
+    while left < right:
+        mid = left + (right - left) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
